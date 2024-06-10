2a92dd609458abd055455a8ed8fb56fe20ed901b
Max Bishop <35214898+MaxBish@users.noreply.github.com>
2024-06-10 14:43:07 -0700

    Update Kandji.py

--- Kandji.py
+++ Kandji.py
@@ -1,1 +1,217 @@
+################################################################################################
+# License Information
+################################################################################################
+#
+# Copyright 2023 Kandji, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+# to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+################################################################################################
+
+import sys
+
 import requests
+from requests.adapters import HTTPAdapter
+
+import json
+import runzero
+from runzero.client import AuthError
+from runzero.api import CustomAssets, CustomIntegrationsAdmin, Sites
+from runzero.types import (
+    CustomAttribute,
+    ImportAsset,
+    IPv4Address,
+    IPv6Address,
+    NetworkInterface,
+    ImportTask,
+)
+
+SUBDOMAIN = "INSERT SUBDOMAIN"  # bravewaffles, example, company_name
+
+# us("") or eu - this can be found in the Kandji settings on the Access tab
+REGION = ""
+
+# Kandji Bearer Token
+TOKEN = ""
+
+# runZero information
+RUNZERO_CLIENT_ID = "runZero client ID"
+RUNZERO_CLIENT_SECRET = "runZero client secret"
+RUNZERO_BASE_URL = "https://console.runZero.com/api/v1.0"
+RUNZERO_ORG_ID = "runZero ORG ID"
+RUNZERO_SITE_NAME = "runZero Site Name"
+
+
+# Kandji API base URL
+if REGION in ["", "us"]:
+    BASE_URL = f"https://{SUBDOMAIN}.api.kandji.io/api"
+
+elif REGION in ["eu"]:
+    BASE_URL = f"https://{SUBDOMAIN}.api.{REGION}.kandji.io/api"
+
+else:
+    sys.exit(f'\nUnsupported region "{REGION}". Please update and try again\n')
+
+# API headers used in the requests
+HEADERS = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Accept": "application/json",
+    "Content-Type": "application/json;charset=utf-8",
+    "Cache-Control": "no-cache",
+}
+
+
+def http_errors(resp, resp_code, err_msg):
+    """Handle HTTP errors."""
+    # 400
+    if resp_code == requests.codes["bad_request"]:
+        print(f"\n\t{err_msg}")
+        print(f"\tResponse msg: {resp.text}\n")
+    # 401
+    elif resp_code == requests.codes["unauthorized"]:
+        print("Make sure that you have the required permissions to access this data.")
+        print(
+            "Depending on the API platform this could mean that access has just been "
+            "blocked."
+        )
+        sys.exit(f"\t{err_msg}")
+    # 403
+    elif resp_code == requests.codes["forbidden"]:
+        print("The api key may be invalid or missing.")
+        sys.exit(f"\t{err_msg}")
+    # 404
+    elif resp_code == requests.codes["not_found"]:
+        print("\nWe cannot find the one that you are looking for...")
+        print("Move along...")
+        print(f"\tError: {err_msg}")
+        print(f"\tResponse msg: {resp}")
+        print(
+            "\tPossible reason: If this is a device it could be because the device is "
+            "not longer\n"
+            "\t\t\t enrolled in Kandji. This would prevent the MDM command from being\n"
+            "\t\t\t sent successfully.\n"
+        )
+    # 429
+    elif resp_code == requests.codes["too_many_requests"]:
+        print("You have reached the rate limit ...")
+        print("Try again later ...")
+        sys.exit(f"\t{err_msg}")
+    # 500
+    elif resp_code == requests.codes["internal_server_error"]:
+        print("The service is having a problem...")
+        sys.exit(err_msg)
+    # 503
+    elif resp_code == requests.codes["service_unavailable"]:
+        print("Unable to reach the service. Try again later...")
+    else:
+        print("Something really bad must have happened...")
+        print(err_msg)
+        sys.exit()
+
+
+def kandji_api(method, endpoint, params=None, payload=None):
+    """Make an API request and return data.
+
+    method   - an HTTP Method (GET, POST, PATCH, DELETE).
+    endpoint - the API URL endpoint to target.
+    params   - optional parameters can be passed as a dict.
+    payload  - optional payload is passed as a dict and used with PATCH and POST
+               methods.
+    Returns a JSON data object.
+    """
+    attom_adapter = HTTPAdapter(max_retries=3)
+    session = requests.Session()
+    session.mount(BASE_URL, attom_adapter)
+
+    try:
+        response = session.request(
+            method,
+            BASE_URL + endpoint,
+            data=payload,
+            headers=HEADERS,
+            params=params,
+            timeout=30,
+        )
+
+        # If a successful status code is returned (200 and 300 range)
+        if response:
+            try:
+                data = response.json()
+            except Exception:
+                data = response.text
+
+        # if the request is successful exceptions will not be raised
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException as err:
+        http_errors(resp=response, resp_code=response.status_code, err_msg=err)
+        data = {"error": f"{response.status_code}", "api resp": f"{err}"}
+
+    return data
+
+
+def get_devices(params=None, ordering="serial_number"):
+    """Return device inventory."""
+    count = 0
+    # limit - set the number of records to return per API call
+    limit = 300
+    # offset - set the starting point within a list of resources
+    offset = 0
+    # inventory
+    data = []
+
+    while True:
+        # update params
+        params.update(
+            {"ordering": f"{ordering}", "limit": f"{limit}", "offset": f"{offset}"}
+        )
+
+        # check to see if a platform was specified
+        response = kandji_api(method="GET", endpoint="/v1/devices", params=params)
+
+        count += len(response)
+        offset += limit
+        if len(response) == 0:
+            break
+
+        # breakout the response then append to the data list
+        for record in response:
+            data.append(record)
+
+    if len(data) < 1:
+        print("No devices found...\n")
+        sys.exit()
+
+    return data
+
+
+def main():
+    """Do the main logic."""
+    print("")
+    print(f"Base URL: {BASE_URL}")
+    print("")
+
+    # dict placeholder for params passed to api requests
+    params_dict = {}
+
+    # Get the total number of devices
+    device_inventory = get_devices(params=params_dict)
+    print(f"Total number of devices: {len(device_inventory)}")
+
+
+if __name__ == "__main__":
+    main()
