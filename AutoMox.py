aa8fa93197235af7c409d8535df3a4ffd6b58639
Max Bishop <35214898+MaxBish@users.noreply.github.com>
2024-08-27 15:43:13 -0700

    Update AutoMox.py

--- AutoMox.py
+++ AutoMox.py
@@ -3,7 +3,6 @@
 import sys
 
 import requests
-from requests.adapters import HTTPAdapter
 
 import json
 import runzero
@@ -31,124 +30,56 @@ RUNZERO_BASE_URL = "https://console.runZero.com/api/v1.0"
 RUNZERO_ORG_ID = "runZero ORG ID"
 RUNZERO_SITE_NAME = "runZero Site Name"
 
-# API headers used in the requests
+## API headers used in the requests
 HEADERS = {
     "Authorization": f"Bearer {TOKEN}"
 }
 
-
-def http_errors(resp, resp_code, err_msg):
-    """Handle HTTP errors."""
-    # 400
-    if resp_code == requests.codes["bad_request"]:
-        print(f"\n\t{err_msg}")
-        print(f"\tResponse msg: {resp.text}\n")
-    # 401
-    elif resp_code == requests.codes["unauthorized"]:
-        print("Make sure that you have the required permissions to access this data.")
-        print(
-            "Depending on the API platform this could mean that access has just been "
-            "blocked."
-        )
-        sys.exit(f"\t{err_msg}")
-    # 403
-    elif resp_code == requests.codes["forbidden"]:
-        print("The api key may be invalid or missing.")
-        sys.exit(f"\t{err_msg}")
-    # 404
-    elif resp_code == requests.codes["not_found"]:
-        print("\nWe cannot find the one that you are looking for...")
-        print("Move along...")
-        print(f"\tError: {err_msg}")
-        print(f"\tResponse msg: {resp}")
-        print(
-            "\tPossible reason: If this is a device it could be because the device is "
-            "not longer\n"
-            "\t\t\t enrolled in Kandji. This would prevent the MDM command from being\n"
-            "\t\t\t sent successfully.\n"
-        )
-    # 429
-    elif resp_code == requests.codes["too_many_requests"]:
-        print("You have reached the rate limit ...")
-        print("Try again later ...")
-        sys.exit(f"\t{err_msg}")
-    # 500
-    elif resp_code == requests.codes["internal_server_error"]:
-        print("The service is having a problem...")
-        sys.exit(err_msg)
-    # 503
-    elif resp_code == requests.codes["service_unavailable"]:
-        print("Unable to reach the service. Try again later...")
-    else:
-        print("Something really bad must have happened...")
-        print(err_msg)
-        sys.exit()
-
-
-def kandji_api(method, endpoint, params=None, payload=None):
+def automox_api(query):
     """Make an API request and return data.
-
-    method   - an HTTP Method (GET, POST, PATCH, DELETE).
-    endpoint - the API URL endpoint to target.
-    params   - optional parameters can be passed as a dict.
-    payload  - optional payload is passed as a dict and used with PATCH and POST
-               methods.
+    params - query parameters
     Returns a JSON data object.
     """
-    attom_adapter = HTTPAdapter(max_retries=3)
-    session = requests.Session()
-    session.mount(BASE_URL, attom_adapter)
 
-    try:
-        response = session.request(
-            method,
-            BASE_URL + endpoint,
-            data=payload,
-            headers=HEADERS,
-            params=params,
-            timeout=30,
-        )
+    response = requests.get(url=AUTOMOX_URL, headers=HEADERS, params=query)
 
-        # If a successful status code is returned (200 and 300 range)
-        if response:
-            try:
-                data = response.json()
-            except Exception:
-                data = response.text
-
-        # if the request is successful exceptions will not be raised
-        response.raise_for_status()
-
-    except requests.exceptions.RequestException as err:
-        http_errors(resp=response, resp_code=response.status_code, err_msg=err)
-        data = {"error": f"{response.status_code}", "api resp": f"{err}"}
+    data = response.json()
 
     return data
 
 
-def get_devices(params=None, ordering="device_name"):
+def get_devices():
     """Return device inventory."""
-    count = 0
-    # limit - set the number of records to return per API call
-    limit = 300
-    # offset - set the starting point within a list of resources
-    offset = 0
+
+    ## Automox query parameters when making API call
+    query = {
+      "groupId": "0",
+      "PS_VERSION": "0",
+      "o": "0",
+      "pending": "0",
+      "patchStatus": "missing",
+      "policyId": "0",
+      "exception": "0",
+      "managed": "0",
+      "limit": "500",
+      "page": "0",
+      "filters[is_compatible]": "true",
+      "sortColumns[]": "is_compatible",
+      "sortDir": "asc"
+    }
+
     # inventory
     data = []
 
     while True:
-        # update params
-        params.update(
-            {"ordering": f"{ordering}", "limit": f"{limit}", "offset": f"{offset}"}
-        )
-
         # check to see if a platform was specified
-        response = kandji_api(method="GET", endpoint="/v1/devices", params=params)
-
-        count += len(response)
-        offset += limit
+        response = automox_api(query)
+        
         if len(response) == 0:
             break
+
+        else:
+            query["page"] = query["page"] + 1
 
         # breakout the response then append to the data list
         for record in response:
@@ -162,25 +93,28 @@ def get_devices(params=None, ordering="device_name"):
 
 
 def build_assets():
+  all_endpoints = get_devices()
 
-  params_dict = {}
-    
-  all_endpoints = get_devices(params=params_dict)
   assets = []
   for endpoint in all_endpoints:
       custom_attrs = {}
       custom_attrs['os_version'] = endpoint['os_version']
-      custom_attrs['agent_installed'] = endpoint['agent_installed']
+      custom_attrs['os_name'] = endpoint['os_name']
+      custom_attrs['os_family'] = endpoint['os_family']
       custom_attrs['agent_version'] = endpoint['agent_version']
 
       mac_address = None
-      if len(endpoint['mac_address']) > 0:
-         mac_address = endpoint['mac_address'][0]
+      if len(endpoint['detail']['NICS']['MAC']) > 0:
+         mac_address = endpoint['detail']['NICS']['MAC']
+
+      ## handle IPs
+      ips = []
+      ips.append(endpoint['ip_addrs'])
 
       assets.append(ImportAsset(
-         id=endpoint['device_id'],
-         networkInterfaces=[build_network_interface(ips=endpoint['ip'] + endpoint['ipv6'],mac=mac_address)],
-         hostnames=[endpoint['device_name']],
+         id=endpoint['id'],
+         networkInterfaces=[build_network_interface(ips=ips,mac=mac_address)],
+         hostnames=[endpoint['name']],
          os_version=endpoint['os_version'],
          customAttributes=custom_attrs
       ))
@@ -235,7 +169,7 @@ def import_data_to_runzero(assets: List[ImportAsset]):
 
     # get or create the custom source manager and create a new custom source
     custom_source_mgr = CustomIntegrationsAdmin(c)
-    my_asset_source = custom_source_mgr.get(name="kandji")
+    my_asset_source = custom_source_mgr.get(name="automox")
     if my_asset_source:
         source_id = my_asset_source.id
     else:
@@ -249,7 +183,7 @@ def import_data_to_runzero(assets: List[ImportAsset]):
         site_id=site.id,
         custom_integration_id=source_id,
         assets=assets,
-        task_info=ImportTask(name="Kandji Sync"),
+        task_info=ImportTask(name="Automox Sync"),
     )
 
     if import_task:
@@ -261,7 +195,7 @@ def import_data_to_runzero(assets: List[ImportAsset]):
 def main():
     """Do the main logic."""
     print("")
-    print(f"Base URL: {BASE_URL}")
+    print(f"Automox URL: " + AUTOMOX_URL)
     print("")
 
     # Get the total number of devices
