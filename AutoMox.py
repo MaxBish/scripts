d0557720442321ffa90fbc6ad79722b9999db5bc
Max Bishop <35214898+MaxBish@users.noreply.github.com>
2024-08-27 15:07:52 -0700

    Update AutoMox.py

--- AutoMox.py
+++ AutoMox.py
@@ -1,1 +1,276 @@
+## Automox!
 
+import sys
+
+import requests
+from requests.adapters import HTTPAdapter
+
+import json
+import runzero
+from runzero.client import AuthError
+from runzero.api import CustomAssets, CustomIntegrationsAdmin, Sites
+from runzero.types import (
+    CustomAttribute,
+    ImportAsset,
+    IPv4Address,
+    IPv6Address,
+    NetworkInterface,
+    ImportTask,
+)
+
+## Automox API URL
+AUTOMOX_URL = "https://console.automox.com/api/servers"
+
+## Automox Bearer Token (can also use os.environ variable)
+TOKEN = ""
+
+# runZero information
+RUNZERO_CLIENT_ID = "runZero client ID"
+RUNZERO_CLIENT_SECRET = "runZero client secret"
+RUNZERO_BASE_URL = "https://console.runZero.com/api/v1.0"
+RUNZERO_ORG_ID = "runZero ORG ID"
+RUNZERO_SITE_NAME = "runZero Site Name"
+
+# API headers used in the requests
+HEADERS = {
+    "Authorization": f"Bearer {TOKEN}"
+}
+
+
+def http_errors(resp, resp_code, err_msg):
+    """Handle HTTP errors."""
+    # 400
+    if resp_code == requests.codes["bad_request"]:
+        print(f"\n\t{err_msg}")
+        print(f"\tResponse msg: {resp.text}\n")
+    # 401
+    elif resp_code == requests.codes["unauthorized"]:
+        print("Make sure that you have the required permissions to access this data.")
+        print(
+            "Depending on the API platform this could mean that access has just been "
+            "blocked."
+        )
+        sys.exit(f"\t{err_msg}")
+    # 403
+    elif resp_code == requests.codes["forbidden"]:
+        print("The api key may be invalid or missing.")
+        sys.exit(f"\t{err_msg}")
+    # 404
+    elif resp_code == requests.codes["not_found"]:
+        print("\nWe cannot find the one that you are looking for...")
+        print("Move along...")
+        print(f"\tError: {err_msg}")
+        print(f"\tResponse msg: {resp}")
+        print(
+            "\tPossible reason: If this is a device it could be because the device is "
+            "not longer\n"
+            "\t\t\t enrolled in Kandji. This would prevent the MDM command from being\n"
+            "\t\t\t sent successfully.\n"
+        )
+    # 429
+    elif resp_code == requests.codes["too_many_requests"]:
+        print("You have reached the rate limit ...")
+        print("Try again later ...")
+        sys.exit(f"\t{err_msg}")
+    # 500
+    elif resp_code == requests.codes["internal_server_error"]:
+        print("The service is having a problem...")
+        sys.exit(err_msg)
+    # 503
+    elif resp_code == requests.codes["service_unavailable"]:
+        print("Unable to reach the service. Try again later...")
+    else:
+        print("Something really bad must have happened...")
+        print(err_msg)
+        sys.exit()
+
+
+def kandji_api(method, endpoint, params=None, payload=None):
+    """Make an API request and return data.
+
+    method   - an HTTP Method (GET, POST, PATCH, DELETE).
+    endpoint - the API URL endpoint to target.
+    params   - optional parameters can be passed as a dict.
+    payload  - optional payload is passed as a dict and used with PATCH and POST
+               methods.
+    Returns a JSON data object.
+    """
+    attom_adapter = HTTPAdapter(max_retries=3)
+    session = requests.Session()
+    session.mount(BASE_URL, attom_adapter)
+
+    try:
+        response = session.request(
+            method,
+            BASE_URL + endpoint,
+            data=payload,
+            headers=HEADERS,
+            params=params,
+            timeout=30,
+        )
+
+        # If a successful status code is returned (200 and 300 range)
+        if response:
+            try:
+                data = response.json()
+            except Exception:
+                data = response.text
+
+        # if the request is successful exceptions will not be raised
+        response.raise_for_status()
+
+    except requests.exceptions.RequestException as err:
+        http_errors(resp=response, resp_code=response.status_code, err_msg=err)
+        data = {"error": f"{response.status_code}", "api resp": f"{err}"}
+
+    return data
+
+
+def get_devices(params=None, ordering="device_name"):
+    """Return device inventory."""
+    count = 0
+    # limit - set the number of records to return per API call
+    limit = 300
+    # offset - set the starting point within a list of resources
+    offset = 0
+    # inventory
+    data = []
+
+    while True:
+        # update params
+        params.update(
+            {"ordering": f"{ordering}", "limit": f"{limit}", "offset": f"{offset}"}
+        )
+
+        # check to see if a platform was specified
+        response = kandji_api(method="GET", endpoint="/v1/devices", params=params)
+
+        count += len(response)
+        offset += limit
+        if len(response) == 0:
+            break
+
+        # breakout the response then append to the data list
+        for record in response:
+            data.append(record)
+
+    if len(data) < 1:
+        print("No devices found...\n")
+        sys.exit()
+
+    return data
+
+
+def build_assets():
+
+  params_dict = {}
+    
+  all_endpoints = get_devices(params=params_dict)
+  assets = []
+  for endpoint in all_endpoints:
+      custom_attrs = {}
+      custom_attrs['os_version'] = endpoint['os_version']
+      custom_attrs['agent_installed'] = endpoint['agent_installed']
+      custom_attrs['agent_version'] = endpoint['agent_version']
+
+      mac_address = None
+      if len(endpoint['mac_address']) > 0:
+         mac_address = endpoint['mac_address'][0]
+
+      assets.append(ImportAsset(
+         id=endpoint['device_id'],
+         networkInterfaces=[build_network_interface(ips=endpoint['ip'] + endpoint['ipv6'],mac=mac_address)],
+         hostnames=[endpoint['device_name']],
+         os_version=endpoint['os_version'],
+         customAttributes=custom_attrs
+      ))
+  return assets
+
+def build_network_interface(ips: List[str], mac: str = None) -> NetworkInterface:
+    """
+    This function converts a mac and a list of strings in either ipv4 or ipv6 format and creates a NetworkInterface that
+    is accepted in the ImportAsset
+    """
+    ip4s: List[IPv4Address] = []
+    ip6s: List[IPv6Address] = []
+    for ip in ips[:99]:
+        try:
+            ip_addr = ip_address(ip)
+            if ip_addr.version == 4:
+                ip4s.append(ip_addr)
+            elif ip_addr.version == 6:
+                ip6s.append(ip_addr)
+            else:
+                continue
+        except:
+            continue
+
+    if mac is None:
+        return NetworkInterface(ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+    else:
+        return NetworkInterface(macAddress=mac, ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+
+
+def import_data_to_runzero(assets: List[ImportAsset]):
+    """
+    The code below gives an example of how to create a custom source and upload valid assets from a CSV to a site using
+    the new custom source.
+    """
+    # create the runzero client
+    c = runzero.Client()
+
+    # try to log in using OAuth credentials
+    try:
+        c.oauth_login(RUNZERO_CLIENT_ID, RUNZERO_CLIENT_SECRET)
+    except AuthError as e:
+        print(f"login failed: {e}")
+        return
+
+    # create the site manager to get our site information
+    site_mgr = Sites(c)
+    site = site_mgr.get(RUNZERO_ORG_ID, RUNZERO_SITE_NAME)
+    if not site:
+        print(f"unable to find requested site")
+        return
+
+    # get or create the custom source manager and create a new custom source
+    custom_source_mgr = CustomIntegrationsAdmin(c)
+    my_asset_source = custom_source_mgr.get(name="kandji")
+    if my_asset_source:
+        source_id = my_asset_source.id
+    else:
+        my_asset_source = custom_source_mgr.create(name="kandji")
+        source_id = my_asset_source.id
+
+    # create the import manager to upload custom assets
+    import_mgr = CustomAssets(c)
+    import_task = import_mgr.upload_assets(
+        org_id=RUNZERO_ORG_ID,
+        site_id=site.id,
+        custom_integration_id=source_id,
+        assets=assets,
+        task_info=ImportTask(name="Kandji Sync"),
+    )
+
+    if import_task:
+        print(
+            f"task created! view status here: https://console.runzero.com/tasks?task={import_task.id}"
+        )
+
+
+def main():
+    """Do the main logic."""
+    print("")
+    print(f"Base URL: {BASE_URL}")
+    print("")
+
+    # Get the total number of devices
+    device_inventory = build_assets()
+    print(f"Total number of devices: {len(device_inventory)}")
+    
+    import_data_to_runzero(device_inventory)
+    
+
+
+if __name__ == "__main__":
+    main()
