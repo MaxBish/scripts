5e5054a0cc7f67b41f9bd44747a0131283c73f81
Max Bishop <maxbishop97@gmail.com>
2024-10-21 10:44:59 -0700

    Updated CB script

--- CarbonBlack.py
+++ CarbonBlack.py
@@ -1,20 +1,33 @@
-import time
-import datetime
-import uuid
-import os
+## CarbonBlack!
+
+## importing dependencies
+from ipaddress import ip_address
+import sys
+import requests
+
+import json
 import runzero
-from runzero.api import CustomAssets, CustomIntegrationsAdmin, Sites, Tasks
-from runzero.types import ImportAsset, ImportTask, NetworkInterface
+from runzero.client import AuthError
+from runzero.api import CustomAssets, CustomIntegrationsAdmin, Sites
+from runzero.types import (
+    CustomAttribute,
+    ImportAsset,
+    IPv4Address,
+    IPv6Address,
+    NetworkInterface,
+    ImportTask,
+)
+
 from cbc_sdk.helpers import build_cli_parser, get_cb_cloud_object
-from cbc_sdk.platform import Device, Vulnerability
+from cbc_sdk.platform import Device
 
 
 # API keys are required for using the runZero sdk. See https://www.runzero.com/docs/leveraging-the-api/
-MY_CLIENT_ID = ""  # OAuth client id. See https://console.runzero.com/account/api/clients
-MY_CLIENT_SECRET = os.environ['RUNZERO_OAUTH_SECRET']  # OAuth client secret. See https://console.runzero.com/account/api/clients
-MY_ORG_ID = ""  # runZero organization ID. See https://console.runzero.com/organizations
-WANTED_SITE_NAME = ""  # Name of site within the above Organization. See https://console.runzero.com/sites
-MY_INTEGRATION_ID = "" # runZero Integration ID # See https://console.runzero.com/custom-integrations
+RUNZERO_CLIENT_ID = ""  # OAuth client id. See https://console.runzero.com/account/api/clients
+RUNZERO_CLIENT_SECRET = os.environ['RUNZERO_OAUTH_SECRET']  # OAuth client secret. See https://console.runzero.com/account/api/clients
+RUNZERO_ORG_ID = ""  # runZero organization ID. See https://console.runzero.com/organizations
+RUNZERO_SITE_NAME = ""  # Name of site within the above Organization. See https://console.runzero.com/sites
+RUNZERO_BASE_URL = "https://console.runZero.com/api/v1.0"
 
 def get_assets():
 
@@ -24,49 +37,106 @@ def get_assets():
     args = parser.parse_args()    
     cb = get_cb_cloud_object(args)
     query = cb.select(Device)
-    #query = query.where("GEI-PC0321")
     devices = list(query)
     print("Got a list of " + str(len(devices)) + " Devices from Carbon Black API")
 
     for device in devices:
-        if not device.name:
-            continue
+        custom_attrs = {}
+        custom_attrs['os_version'] = device.os_version
+        custom_attrs['os'] = device.os
 
-        print(str(device.id) + " " + device.name or "None")
-        #print(device)
-        
-        nwints = []
-        nwint = NetworkInterface()
-        #if device.mac_address:
-        #    nwint.mac_address = ':'.join(device.mac_address[i:i+2] for i in range(0,12,2))
-        if device.last_internal_ip_address and ':' in device.last_internal_ip_address:
-            nwint.ipv6_addresses = [device.last_internal_ip_address]
-        if device.last_internal_ip_address and '.' in device.last_internal_ip_address:
-            nwint.ipv4_addresses = [device.last_internal_ip_address]
-        #print(nwint)
-        nwints.append(nwint)
+        mac_address = None
+        if device.mac_address is not None:
+            mac_address = device.mac_address
 
-        importasset = ImportAsset(
-            id                 = device.id,
-            network_interfaces = nwints,
-            os                 = device.os,
-            os_version         = device.os_version,
-            first_seen_ts      = device.registered_time
-            #custom_attributes = custom_attributes
-        )
+        ips = []
+        ips.append(device.last_internal_ip_address)
+        ips.append(device.last_external_ip_address)
 
-        print(device.name)
-        if '\\' in device.name:
-            hostname = device.name.split('\\')[1]
-            importasset.domain = device.name.split('\\')[0]
-        importasset.hostnames = [hostname]
-        
-        rz_import_assets.append(importasset)
-
-#    software: Optional[List[AssetSoftware]] = Field(None, max_items=1000)
-#    vulnerabilities: Optional[List[AssetVulnerability]] = Field(None, max_items=1000)
+        rz_import_assets.append(ImportAsset(
+            id=device.id,
+            networkInterfaces=[build_network_interface(ips=ips,mac=mac_address)],
+            hostnames=device.name,
+            firstSeenTS=device.registered_time,
+            os=device.os,
+            osVersion=device.os_version
+            customAttributes=custom_attrs
+            ))
 
     return rz_import_assets
+
+
+
+def build_network_interface(ips: list[str], mac: str = None) -> NetworkInterface:
+    """
+    This function converts a mac and a list of strings in either ipv4 or ipv6 format and creates a NetworkInterface that
+    is accepted in the ImportAsset
+    """
+    ip4s: list[IPv4Address] = []
+    ip6s: list[IPv6Address] = []
+    for ip in ips[:99]:
+            ip_addr = ip_address(ip[0])
+            if ip_addr.version == 4:
+                ip4s.append(ip_addr)
+            elif ip_addr.version == 6:
+                ip6s.append(ip_addr)
+            else:
+                continue
+
+    if mac is None:
+        return NetworkInterface(ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+    else:
+        return NetworkInterface(macAddress=mac, ipv4Addresses=ip4s, ipv6Addresses=ip6s)
+    
+
+
+
+def import_data_to_runzero(assets: list[ImportAsset])
+    """
+    The code below gives an example of how to create a custom source and upload valid assets from a CSV to a site using
+    the new custom source.
+    """
+    # create the runzero client
+    c = runzero.Client()
+
+    # try to log in using OAuth credentials
+    try:
+        c.oauth_login(RUNZERO_CLIENT_ID, RUNZERO_CLIENT_SECRET)
+    except AuthError as e:
+        print(f"login failed: {e}")
+        return
+
+    # create the site manager to get our site information
+    site_mgr = Sites(c)
+    site = site_mgr.get(RUNZERO_ORG_ID, RUNZERO_SITE_NAME)
+    if not site:
+        print(f"unable to find requested site")
+        return
+
+    # get or create the custom source manager and create a new custom source
+    custom_source_mgr = CustomIntegrationsAdmin(c)
+    my_asset_source = custom_source_mgr.get(name="CarbonBlack")
+    if my_asset_source:
+        source_id = my_asset_source.id
+    else:
+        my_asset_source = custom_source_mgr.create(name="CarbonBlack")
+        source_id = my_asset_source.id
+
+    # create the import manager to upload custom assets
+    import_mgr = CustomAssets(c)
+    import_task = import_mgr.upload_assets(
+        org_id=RUNZERO_ORG_ID,
+        site_id=site.id,
+        custom_integration_id=source_id,
+        assets=assets,
+        task_info=ImportTask(name="CarbonBlack Sync"),
+    )
+
+    if import_task:
+        print(
+            f"Task created! View status here: https://console.runzero.com/tasks?task={import_task.id}"
+        )
+
 
 
 def main():
@@ -74,35 +144,16 @@ def main():
     The code below uploads assets from the get_assets function to runzero
     source.
     """
-    # create the runzero client
-    c = runzero.Client()
+    print("")
+    print("Running CarbonBlack Integration")
+    print("")
 
-    # try to log in using OAuth credentials
-    try:
-        c.oauth_login(MY_CLIENT_ID, MY_CLIENT_SECRET)
-    except runzero.AuthError as e:
-        print(f"login failed: {e}")
-        return
-    print("login successful")
+    ## Get all the devices
+    device_inventory = get_assets()
+    print(f"Total number of devices: {len(device_inventory)}")
 
-    # create the site api manager to get our site information
-    site_mgr = Sites(c)
-    site = site_mgr.get(MY_ORG_ID, WANTED_SITE_NAME)
-    if not site:
-        print(f"unable to find requested site")
-        return
-    print(f"got information for site {site.name}")
+    import_data_to_runzero(device_inventory)
 
-    # creates some example assets
-    assets = get_assets()
-
-    # create our named import task with 'exclude unknown' set to true
-    task_options = ImportTask(name=f'CB - {datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")}', exclude_unknown=True)
-
-    # create the import api manager to upload custom assets
-    import_mgr = CustomAssets(c)
-    import_task = import_mgr.upload_assets(MY_ORG_ID, site.id, MY_INTEGRATION_ID, assets, task_info=task_options)
-    print(f"created an custom asset import task: {import_task.name}")
 
 if __name__ == "__main__":
     main()
